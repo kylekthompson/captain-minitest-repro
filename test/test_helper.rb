edd14a863a0d74de80b39dfa0f638a4834fd7a5b
Kyle Thompson <me@kkt.dev>
2024-10-28 10:10:50 -0400

    Add minitest-reporters

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,12 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
   end
 end
+
+require "minitest/reporters"
+
+Minitest::Reporters.use!(
+  [
+    Minitest::Reporters::DefaultReporter.new,
+    Minitest::Reporters::JUnitReporter.new("tmp/reports", true, single_file: true)
+  ]
+)
